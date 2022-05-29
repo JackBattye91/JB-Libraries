013482a51df0a848ea58dec4beeb4f5eda13bc27
JackBattye91 <jack.battye@gmail.com>
2022-05-29 11:00:03 +0100

    Added ErrorWorker class

---
+++ Common/ErrorWorker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Common {
+    public class ErrorWorker {
+        public static void AddError<T>(IReturnCode<T> rc, long pErrorCode) {
+            AddError(rc, pErrorCode, null, null);
+        }
+        public static void AddError<T>(IReturnCode<T> rc, long pErrorCode, string? pMessage) {
+            AddError(rc, pErrorCode, pMessage, null);
+        }
+        public static void AddError<T>(IReturnCode<T> rc, long pErrorCode, string? pMessage, string? pStackTrace) {
+            rc.Errors.Add(new Error() { 
+                    ErrorCode = pErrorCode, 
+                    Message = pMessage ?? String.Empty,
+                    StackTrace = pStackTrace ?? Environment.StackTrace
+                });
+        }
+
+        public static void CopyErrorCode<T, U>(IReturnCode<T> pSource, IReturnCode<U> pDestination) {
+            pDestination.ErrorCode = pSource.ErrorCode;
+
+            foreach(var srcCodes in pSource.Errors) {
+                pDestination.Errors.Add(srcCodes);
+        }
+    }
+}
