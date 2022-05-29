e42e7f1e737d6f54826fd5684358df528ff58f2a
JackBattye91 <jack.battye@gmail.com>
2022-05-29 17:32:32 +0100

    Added Id property to CalendarEvent

--- Calendar/GoogleCalendar/Models/CalendarEvent.cs
+++ Calendar/GoogleCalendar/Models/CalendarEvent.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace JB.Calendar.GoogleCalendar.Models {
     internal class CalendarEvent : Interfaces.ICalendarEvent {
+        public string Id { get; set; }
         public DateTime? Start { get; set; }
         public DateTime? Finish { get; set; }
+
+        public CalendarEvent() {
+            Id = String.Empty;
+        }
     }
 }
--- Calendar/Interfaces/ICalendarEvent.cs
+++ Calendar/Interfaces/ICalendarEvent.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace JB.Calendar.Interfaces {
     public interface ICalendarEvent {
+        string Id { get; set; }
         DateTime? Start { get; set; }
         DateTime? Finish { get; set; }
     }
