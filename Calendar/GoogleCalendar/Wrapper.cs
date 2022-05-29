c06ca986e48f0bcff45b4c9cd80600d55a867240
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-05-29 14:10:29 +0100

    Added calendar wrapper

---
+++ Calendar/GoogleCalendar/Wrapper.cs
@@ -0,0 +1,34 @@
+﻿using JB.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+
+namespace JB.Calendar.GoogleCalendar {
+    internal class Wrapper : IWrapper {
+        UserCredential? userCredential;
+
+        public Wrapper() {
+            userCredential = null;
+        }
+
+        public IReturnCode<bool> GetCalendarEvents() {
+            IReturnCode<bool> rc = new ReturnCode<bool>();
+            UserCredential credential;
+
+            var service = new CalendarService(new BaseClientService.Initializer() {
+                HttpClientInitializer = credential,
+                ApplicationName = Consts.APPLICATION_NAME
+            });
+
+            return rc;
+        }
+    }
+}
---
+++ Calendar/IWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Calendar {
+    public interface IWrapper {
+        public JB.Common.IReturnCode<bool> GetCalendarEvents();
+    }
+}
