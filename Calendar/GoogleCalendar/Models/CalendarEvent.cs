918dc5ab6eaf2d36593eb2ab904a582d335efe38
JackBattye91 <jack.battye@gmail.com>
2022-05-29 17:03:51 +0100

    Added Calendar Event class

---
+++ Calendar/GoogleCalendar/Models/CalendarEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Calendar.GoogleCalendar.Models {
+    internal class CalendarEvent : Interfaces.ICalendarEvent {
+        public DateTime? Start { get; set; }
+        public DateTime? Finish { get; set; }
+    }
+}
