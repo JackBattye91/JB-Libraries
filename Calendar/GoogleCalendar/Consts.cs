5d15aaf5023a883da17ef0d52e027c86b42887e3
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-05-29 14:10:39 +0100

    Added Google calendat consts

---
+++ Calendar/GoogleCalendar/Consts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Calendar.GoogleCalendar {
+    internal class Consts {
+        public const string APPLICATION_NAME = "";
+    }
+}
