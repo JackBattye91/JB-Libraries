2c37c541162e81dc58e3bab77422b555d22b535b
JackBattye91 <jack.battye@gmail.com>
2022-05-30 22:38:06 +0100

    Updated Cosmos wrapper

--- Cosmos/Cosmos/Wrapper.cs
+++ Cosmos/Cosmos/Wrapper.cs
@@ -14,59 +14,89 @@ namespace JB.NoSqlDatabase.Cosmos {
             cosmosClient = null;
         }
 
-        public IReturnCode<bool> CreateDatabase(string pDatabaseId) {
-            IReturnCode<bool> rc = new ReturnCode<bool>(ErrorCodes.SUCCESS);
+        public async Task<IReturnCode<bool>> CreateDatabase(string pDatabaseId) {
+            IReturnCode<bool> rc = new ReturnCode<bool>();
             string? connectionString = Environment.GetEnvironmentVariable("cosmos-connection-string");
 
-            cosmosClient = new CosmosClient(connectionString);
-            cosmosClient.CreateDatabaseIfNotExistsAsync(pDatabaseId).Wait();
+            try {
+                cosmosClient = new CosmosClient(connectionString);
+                DatabaseResponse response = await cosmosClient.CreateDatabaseIfNotExistsAsync(pDatabaseId);
+                
+                if (System.Net.HttpStatusCode.OK != response.StatusCode) {
+                    rc.ErrorCode = ErrorCodes.UNABLE_TO_CREATE_DATABASE;
+                    ErrorWorker.AddError(rc, rc.ErrorCode);
+                }
+            }
+            catch (Exception e) {
+                rc.ErrorCode = 7;
+                ErrorWorker.AddError(rc, rc.ErrorCode, e.Message, e.StackTrace);
+            }
+            
 
             return rc;
         }
+        public async Task<IReturnCode<Container>> GetContainer(string pDatabaseId, string pContainerId) {
+            return await GetCosmosContainer(pDatabaseId, pContainerId);
+        }
+        public async Task<IReturnCode<Container>> CreateContainer(string pDataBaseId, string pContainerName, string pPartitionKey) {
+            IReturnCode<Container> rc = new ReturnCode<Container>(JB.Common.ErrorCodes.SUCCESS);
+            Database? database = null;
 
-        public IReturnCode<Container> GetContainer(string pDatabaseID, string pContainerId) {
-            IReturnCode<Container> rc = new ReturnCode<Container>(ErrorCodes.SUCCESS);
+            if (!pPartitionKey.StartsWith('/')) {
+                pPartitionKey = '/' + pPartitionKey;
+            }
 
-            if (ErrorCodes.SUCCESS == rc.ErrorCode) {
-                Container? container = cosmosClient?.GetContainer(pDatabaseID, pContainerId);
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                var getDatabaseRc = await GetCosmosDatabase(pDataBaseId);
 
-                if (null != container) {
-                    rc.Data = container;
+                if (JB.Common.ErrorCodes.SUCCESS == getDatabaseRc.ErrorCode) {
+                    database = getDatabaseRc.Data;
                 }
-                else {
-                    rc.ErrorCode = 1;
-                    ErrorWorker.AddError(rc, rc.ErrorCode);
+                if (JB.Common.ErrorCodes.SUCCESS != getDatabaseRc.ErrorCode) {
+                    ErrorWorker.CopyErrorCode(getDatabaseRc, rc);
+                }
+            }
+
+
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                try {
+                    if (database != null) {
+                        ContainerResponse? response = await database.CreateContainerIfNotExistsAsync(pContainerName, pPartitionKey);
+
+                        if (System.Net.HttpStatusCode.Created != response?.StatusCode &&
+                            System.Net.HttpStatusCode.OK != response.StatusCode) {
+                            rc.ErrorCode = ErrorCodes.UNABLE_TO_CREATE_CONTAINER;
+                            ErrorWorker.AddError(rc, rc.ErrorCode);
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    rc.ErrorCode = 2;
+                    ErrorWorker.AddError(rc, rc.ErrorCode, e.Message, e.StackTrace);
                 }
             }
 
             return rc;
         }
-        public IReturnCode<Container> CreateContainer(string pDataBaseId, string pContainerName) {
-            IReturnCode<Container> rc = new ReturnCode<Container>(JB.Common.ErrorCodes.SUCCESS);
 
-
-
-            return rc;
-        }
         public async Task<IReturnCode<T>> AddItem<T>(string pDatabaseId, string pContainerId, T item) {
-            IReturnCode<T> rc = new ReturnCode<T>(ErrorCodes.SUCCESS);
+            IReturnCode<T> rc = new ReturnCode<T>();
             Container? container = null;
 
-            if (ErrorCodes.SUCCESS == rc.ErrorCode) {
-                var getContainerRc = GetContainer(pDatabaseId, pContainerId);
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                var getContainerRc = await GetCosmosContainer(pDatabaseId, pContainerId);
 
-
-                if (ErrorCodes.SUCCESS == getContainerRc?.ErrorCode) {
+                if (JB.Common.ErrorCodes.SUCCESS == getContainerRc?.ErrorCode) {
                     container = getContainerRc.Data;
                 }
 
-                if (ErrorCodes.SUCCESS != getContainerRc?.ErrorCode) {
+                if (JB.Common.ErrorCodes.SUCCESS != getContainerRc?.ErrorCode) {
                     if (getContainerRc != null)
                         ErrorWorker.CopyErrorCode(getContainerRc, rc);
                 }
             }
 
-            if (ErrorCodes.SUCCESS == rc.ErrorCode) {
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
                 try {
                     if (container != null) {
                         await container.CreateItemAsync(item);
@@ -81,77 +111,194 @@ namespace JB.NoSqlDatabase.Cosmos {
             return rc;
         }
         public async Task<IReturnCode<IList<T>>> GetItems<T>(string pDatabaseId, string pContainerId) {
-            IReturnCode<IList<T>> rc = new ReturnCode<IList<T>>(ErrorCodes.SUCCESS);
+            IReturnCode<IList<T>> rc = new ReturnCode<IList<T>>();
             Container? container = null;
             IList<T> itemsList = new List<T>();
 
-            if (ErrorCodes.SUCCESS == rc.ErrorCode) {
-                var getContainerRc = GetContainer(pDatabaseId, pContainerId);
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                var getContainerRc = await GetCosmosContainer(pDatabaseId, pContainerId);
 
-                if (ErrorCodes.SUCCESS == getContainerRc.ErrorCode) {
+                if (JB.Common.ErrorCodes.SUCCESS == getContainerRc.ErrorCode) {
                     container = getContainerRc.Data;
                 }
 
-                if (ErrorCodes.SUCCESS != getContainerRc.ErrorCode) {
+                if (JB.Common.ErrorCodes.SUCCESS != getContainerRc.ErrorCode) {
                     rc.ErrorCode = getContainerRc.ErrorCode;
                     ErrorWorker.AddError(getContainerRc, getContainerRc.ErrorCode);
                 }
             }
 
-            if (ErrorCodes.SUCCESS == rc.ErrorCode) {
-                QueryDefinition queryDefinition = new QueryDefinition($"SELECT * FROM c");
-                using (FeedIterator<T>? feedIterator = container?.GetItemQueryIterator<T>(queryDefinition)) {
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
 
-                    while (feedIterator?.HasMoreResults == true) {
-                        var resultSet = await feedIterator.ReadNextAsync();
+                try {
+                    QueryDefinition queryDefinition = new QueryDefinition($"SELECT * FROM c");
+                    using (FeedIterator<T>? feedIterator = container?.GetItemQueryIterator<T>(queryDefinition)) {
 
-                        foreach (T? f in resultSet) {
-                            itemsList.Add(f);
+                        while (feedIterator?.HasMoreResults == true) {
+                            var resultSet = await feedIterator.ReadNextAsync();
+
+                            foreach (T? f in resultSet) {
+                                itemsList.Add(f);
+                            }
                         }
                     }
                 }
+                catch (Exception e) {
+                    rc.ErrorCode = 3;
+                    ErrorWorker.AddError(rc, rc.ErrorCode, e.Message, e.StackTrace);
+                }
+                
             }
 
-            if (ErrorCodes.SUCCESS == rc.ErrorCode) {
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
                 rc.Data = itemsList;
             }
 
             return rc;
         }
         public async Task<IReturnCode<T>> GetItem<T>(string pDatabaseId, string pContainerId, string pItemId) {
-            IReturnCode<IList<T>> rc = new ReturnCode<IList<T>>(ErrorCodes.SUCCESS);
+            IReturnCode<T> rc = new ReturnCode<T>();
             Container? container = null;
             IList<T> itemsList = new List<T>();
 
-            if (ErrorCodes.SUCCESS == rc.ErrorCode) {
-                var getContainerRc = GetContainer(pDatabaseId, pContainerId);
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                var getContainerRc = await GetCosmosContainer(pDatabaseId, pContainerId);
 
-                if (ErrorCodes.SUCCESS == getContainerRc.ErrorCode) {
+                if (JB.Common.ErrorCodes.SUCCESS == getContainerRc.ErrorCode) {
                     container = getContainerRc.Data;
                 }
 
-                if (ErrorCodes.SUCCESS != getContainerRc.ErrorCode) {
+                if (JB.Common.ErrorCodes.SUCCESS != getContainerRc.ErrorCode) {
                     rc.ErrorCode = getContainerRc.ErrorCode;
                     ErrorWorker.AddError(getContainerRc, getContainerRc.ErrorCode);
                 }
             }
 
-            if (ErrorCodes.SUCCESS == rc.ErrorCode) {
-                QueryDefinition queryDefinition = new QueryDefinition($"SELECT * FROM c WHERE c.id='{pItemId}';");
-                using (FeedIterator<T>? feedIterator = container?.GetItemQueryIterator<T>(queryDefinition)) {
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                try {
+                    QueryDefinition queryDefinition = new QueryDefinition($"SELECT * FROM c WHERE c.id='{pItemId}'");
+                    using (FeedIterator<T>? feedIterator = container?.GetItemQueryIterator<T>(queryDefinition)) {
 
-                    while (feedIterator?.HasMoreResults == true) {
-                        var resultSet = await feedIterator.ReadNextAsync();
+                        while (feedIterator?.HasMoreResults == true) {
+                            var resultSet = await feedIterator.ReadNextAsync();
 
-                        foreach (T? f in resultSet) {
-                            itemsList.Add(f);
+                            foreach (T? f in resultSet) {
+                                itemsList.Add(f);
+                            }
                         }
                     }
                 }
+                catch (Exception e) {
+                    rc.ErrorCode = 5;
+                    ErrorWorker.AddError(rc, rc.ErrorCode, e.Message, e.StackTrace);
+                }
+                
             }
 
-            if (ErrorCodes.SUCCESS == rc.ErrorCode) {
-                rc.Data = itemsList;
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                rc.Data = itemsList.Count > 0 ? itemsList[0] : default;
+            }
+
+            return rc;
+        }
+        public async Task<IReturnCode<T>> UpdateItem<T>(string pDatabaseId, string pContainerId, T pItem, string pItemId) {
+            IReturnCode<T> rc = new ReturnCode<T>();
+            Container? container = null;
+
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                var getContainerRc = await GetCosmosContainer(pDatabaseId, pContainerId);
+
+                if (JB.Common.ErrorCodes.SUCCESS == getContainerRc?.ErrorCode) {
+                    container = getContainerRc.Data;
+                }
+
+                if (JB.Common.ErrorCodes.SUCCESS != getContainerRc?.ErrorCode) {
+                    if (getContainerRc != null)
+                        ErrorWorker.CopyErrorCode(getContainerRc, rc);
+                }
+            }
+
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                try {
+                    if (container != null) {
+                        ItemResponse<T> response = await container.ReplaceItemAsync(pItem, pItemId, new PartitionKey("/id"));
+                        
+                        if (System.Net.HttpStatusCode.OK != response.StatusCode) {
+                            rc.ErrorCode = JB.Common.ErrorCodes.BAD_HTTP_STATUS_CODE;
+                            ErrorWorker.AddError(rc, rc.ErrorCode);
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    rc.ErrorCode = 2;
+                    ErrorWorker.AddError(rc, rc.ErrorCode, e.Message, e.StackTrace);
+                }
+            }
+
+            return rc;
+        }
+
+        protected async Task<IReturnCode<Database>> GetCosmosDatabase(string pDatabaseId) {
+            IReturnCode<Database> rc = new ReturnCode<Database>();
+            string? connectionString = Environment.GetEnvironmentVariable("cosmos-connection-string");
+            Database? database = null;
+
+            try {
+                cosmosClient = new CosmosClient(connectionString);
+                DatabaseResponse response = await cosmosClient.CreateDatabaseIfNotExistsAsync(pDatabaseId);
+                
+                if (System.Net.HttpStatusCode.OK == response.StatusCode) {
+                    database = response.Database;
+                }
+                else if (System.Net.HttpStatusCode.OK != response.StatusCode) {
+                    rc.ErrorCode = 5;
+                    ErrorWorker.AddError(rc, rc.ErrorCode);
+                }
+            }
+            catch (Exception e) {
+                rc.ErrorCode = 7;
+                ErrorWorker.AddError(rc, rc.ErrorCode, e.Message, e.StackTrace);
+            }
+
+
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                rc.Data = database;
+            }
+
+            return rc;
+        }
+        protected async Task<IReturnCode<Container>> GetCosmosContainer(string pDatabaseId, string pContainerId) {
+            IReturnCode<Container> rc = new ReturnCode<Container>();
+            Database? database = null;
+
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                var databaseRc = await GetCosmosDatabase(pDatabaseId);
+
+                if (JB.Common.ErrorCodes.SUCCESS == databaseRc.ErrorCode) {
+                    database = databaseRc.Data;
+                }
+
+                if (JB.Common.ErrorCodes.SUCCESS != databaseRc.ErrorCode) {
+                    ErrorWorker.CopyErrorCode(databaseRc, rc);
+                }
+            }
+
+            if (JB.Common.ErrorCodes.SUCCESS == rc.ErrorCode) {
+                try {
+                    Container? container = database?.GetContainer(pContainerId);
+
+                    if (null != container) {
+                        rc.Data = container;
+                    }
+                    else {
+                        rc.ErrorCode = ErrorCodes.UNABLE_TO_GET_CONTAINER;
+                        ErrorWorker.AddError(rc, rc.ErrorCode);
+                    }
+                }
+                catch (Exception e) {
+                    rc.ErrorCode = 7;
+                    ErrorWorker.AddError(rc, rc.ErrorCode, e.Message, e.StackTrace);
+                }
             }
 
             return rc;
