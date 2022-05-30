3deefeda9f7ffd3f630de5b5dc560670a6afd3c3
JackBattye91 <jack.battye@gmail.com>
2022-05-30 22:38:25 +0100

    Updated Cosmos IWrapper

--- Cosmos/IWrapper.cs
+++ Cosmos/IWrapper.cs
@@ -7,11 +7,14 @@ using Microsoft.Azure.Cosmos;
 
 namespace JB.NoSqlDatabase {
     public interface IWrapper {
-        public JB.Common.IReturnCode<bool> CreateDatabase(string pDatabaseId);
-        public JB.Common.IReturnCode<Container> GetContainer(string pDatabaseId, string pContainerName);
-        public JB.Common.IReturnCode<Container> CreateContainer(string pDatabaseId, string pContainerName);
+        public Task<JB.Common.IReturnCode<bool>> CreateDatabase(string pDatabaseId);
+       
+        public Task<JB.Common.IReturnCode<Container>> GetContainer(string pDatabaseId, string pContainerName);
+        public Task<JB.Common.IReturnCode<Container>> CreateContainer(string pDatabaseId, string pContainerName, string pPartitionKey);
+        
         public Task<JB.Common.IReturnCode<T>> GetItem<T>(string pDatebaseId, string pContainerId, string pItemId);
-        public Task<JB.Common.IReturnCode<IList<T>>> GetItems<T>(string pDatabaseId, string pTableName);
-        public Task<JB.Common.IReturnCode<T>> AddItem<T>(string pDatabaseId, string pTableName, T pItem);
+        public Task<JB.Common.IReturnCode<IList<T>>> GetItems<T>(string pDatabaseId, string pContainerId);
+        public Task<JB.Common.IReturnCode<T>> AddItem<T>(string pDatabaseId, string pContainerId, T pItem);
+        public Task<JB.Common.IReturnCode<T>> UpdateItem<T>(string pDatabaseId, string pContainerId, T pItem, string pItemId);
     }
 }
