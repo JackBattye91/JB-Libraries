d42b971c0efe2d307711f9f476ce1b31b9472c32
JackBattye91 <jack.battye@gmail.com>
2022-05-30 22:37:51 +0100

    Added error codes to cosmos

---
+++ Cosmos/Cosmos/ErrorCodes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.NoSqlDatabase.Cosmos {
+    internal class ErrorCodes {
+        public const int BASE = 1 * JB.Common.ErrorCodes.API_BASE;
+        public const int LIB = 0 * JB.Common.ErrorCodes.LIBRARY_BASE;
+
+        public const int UNABLE_TO_GET_CONTAINER =      BASE + LIB + 1;
+        public const int UNABLE_TO_CREATE_DATABASE =    BASE + LIB + 2;
+        public const int UNABLE_TO_CREATE_CONTAINER =   BASE + LIB + 3;
+    }
+}
