9d836dd14ca12fd5538e6a78437eec881a731558
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-05-31 20:55:27 +0100

    Added more error codes

--- Calendar/GoogleCalendar/ErrorCodes.cs
+++ Calendar/GoogleCalendar/ErrorCodes.cs
@@ -5,11 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace JB.Calendar.GoogleCalendar {
-    internal class ErrorCodes {
+    public class ErrorCodes {
         // XXX-XXX-XXXX
         // 200 000 0000
-        public const int BASE = 2 * JB.Common.ErrorCodes.API_BASE;
-        public const int LIB = 0 * JB.Common.ErrorCodes.LIBRARY_BASE;
+        private const int BASE = 2 * JB.Common.ErrorCodes.API_BASE;
+        private const int LIB = 0 * JB.Common.ErrorCodes.LIBRARY_BASE;
 
         public const int UNABLE_TO_INSERT_NEW_ITEM =        BASE + LIB + 1;
         public const int UNABLE_TO_UPDATE_ITEM =            BASE + LIB + 2;
