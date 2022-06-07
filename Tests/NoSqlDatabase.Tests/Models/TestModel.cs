1b8c37b6fa89f01ed3dfc15cce4fc59cf099f359
JackBattye91 <jack.battye@gmail.com>
2022-06-07 20:39:28 +0100

    Added test model for NoSqlDatabase testing

---
+++ Tests/NoSqlDatabase.Tests/Models/TestModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace NoSqlDatabase.Tests.Models {
+    internal class TestModel {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public IList<string> TestItems { get; set; }
+
+        public TestModel() {
+            Name = String.Empty;
+            TestItems = new List<string>();
+        }
+    }
+}
