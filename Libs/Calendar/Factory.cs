907335f8c3b36a2f31c421cee2a5ba0d80d9ef03
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-06-07 19:59:29 +0100

    added calendar factor class

---
+++ Libs/Calendar/Factory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Calendar {
+    public class Factory {
+        public static IWrapper CreateCalendarWrapper() {
+            return new GoogleCalendar.Wrapper();
+        }
+    }
+}
