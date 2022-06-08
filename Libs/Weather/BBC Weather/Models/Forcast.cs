77e2a1cb2c5e1990c8aeea56e9b36106a8bd9df4
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-06-08 21:12:57 +0100

    Added forcast class

---
+++ Libs/Weather/BBC Weather/Models/Forcast.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Weather.BBC_Weather.Models {
+    internal class Forcast : Interfaces.IForcast {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public Interfaces.GpsCoordinate? GpsLocation { get; set; }
+        public float? TemeratureCelsius { get; set; }
+        public float? TemeratureFahrenheit { get; set; }
+        public float? WindSpeedMph { get; set; }
+        public string? WindDirection { get; set; }
+        public float? Humidity { get; set; }
+        public float? PressureMb { get; set; }
+
+        public Forcast() {
+            Title = string.Empty;
+            Date = new DateTime();
+        }
+    }
+}
