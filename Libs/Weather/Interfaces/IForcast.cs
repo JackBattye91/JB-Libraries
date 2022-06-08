f893b738a50a7b94fe2208646c29cc54ffbf3911
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-06-08 21:14:18 +0100

    Updated forcast interface

--- Libs/Weather/Interfaces/IForcast.cs
+++ Libs/Weather/Interfaces/IForcast.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace JB.Weather.Interfaces {
     public interface IForcast {
+        string Title { get; set; }
         DateTime Date { get; set; }
-        string Description { get; set; }
-
+        string? Description { get; set; }
+        string? Location { get; set; }
+        IGpsCoordinate? GpsLocation { get; set; }
+        float? TemeratureCelsius { get; set; }
+        float? TemeratureFahrenheit { get; set; }
+        float? MinimumTemeratureCelsius { get; set; }
+        float? MinimumTemeratureFahrenheit { get; set; }
+        float? MaximumTemeratureCelsius { get; set; }
+        float? MaximumTemeratureFahrenheit { get; set; }
+        float? WindSpeedMph { get; set; }
+        string? WindDirection { get; set; }
+        float? Humidity { get; set; }
+        float? PressureMb { get; set; }
     }
 }
