d70f6bd2cc6eb430d13055c6994817101e24f299
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-06-16 18:33:49 +0100

    Added container class to cosmos

---
+++ Libs/Cosmos/Cosmos/Models/Container.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.NoSqlDatabase.Cosmos.Models {
+    internal class Container : Interfaces.IContainer {
+        public string? ContainerId { get; set; }
+        public string? ContainerName { get; set; }
+        public string? PartitionKey { get; set; }
+    }
+}
