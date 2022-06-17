4bb8c8ba761750090408e99e043320352586f15c
JackBattye91 <jack.battye@gmail.com>
2022-06-17 22:53:34 +0100

    Added Sql Database and SQLite

---
+++ Libs/SqlDatabase/IWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JB.Common.Errors;
+
+namespace JB.SqlDatabase {
+    public interface IWrapper {
+        Task<IReturnCode<bool>> CreateDatabase(string pDatabaseName);
+        Task<IReturnCode<bool>> CreateTable(string pDatabaseName, string pTableName);
+
+    }
+}
---
+++ Libs/SqlDatabase/SQlite/Wrapper.cs
@@ -0,0 +1,32 @@
+﻿using JB.Common.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace JB.SqlDatabase.SQlite {
+    internal class Wrapper : IWrapper {
+        public async Task<IReturnCode<bool>> CreateDatabase(string pDatabaseName) {
+            SqliteConnection connection = new SqliteConnection();
+            await connection.OpenAsync();
+
+            var command = connection.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "";
+
+            SqliteDataReader reader = await command.ExecuteReaderAsync();
+            if (await reader.ReadAsync()) {
+                string id = reader["id"] as string ?? string.Empty;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public Task<IReturnCode<bool>> CreateTable(string pDatabaseName, string pTableName) {
+            throw new NotImplementedException();
+        }
+    }
+}
