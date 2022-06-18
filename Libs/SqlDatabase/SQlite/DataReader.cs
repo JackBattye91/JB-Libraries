ad406b70305d4abe06109dd0837e114e389cf914
JackBattye91 <jack.battye@gmail.com>
2022-06-18 21:36:55 +0100

    Added sqlite data reader

---
+++ Libs/SqlDatabase/SQlite/DataReader.cs
@@ -0,0 +1,27 @@
+using JB.Common.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace JB.SqlDatabase.SQlite {
+    internal class DataReader : Interfaces.IDataReader {
+        public bool HasRows { get => return sqlDataReader.HasRows; }
+        protected SqlDataReader sqlDataReader { get; set; }
+
+        public DataReader(SqlDataReader pReader) {
+            sqlDataReader = pReader;
+        }
+
+        public void NextRow() {
+            sqlDataReader.Read();
+        }
+        public object Get(string pName) {
+            int ordinal = sqlDataReader.GetOrdinal(pName);
+            return sqlDataReader.GetValue(ordinal);
+        }
+    }
+}
