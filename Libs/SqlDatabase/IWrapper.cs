68033be7664cf4d85b89a341a7d15b30743528c1
JackBattye91 <jack.battye@gmail.com>
2022-06-18 21:37:26 +0100

    Added run query functions to SQlite

--- Libs/SqlDatabase/IWrapper.cs
+++ Libs/SqlDatabase/IWrapper.cs
@@ -9,6 +9,7 @@ namespace JB.SqlDatabase {
     public interface IWrapper {
         Task<IReturnCode<bool>> CreateDatabase(string pDatabaseName);
         Task<IReturnCode<bool>> CreateTable(string pDatabaseName, string pTableName);
-
+        Task<IReturnCode<Interfaces.IDataReader>> RunQuery(string pDatabaseName, string pQuery);
+        Task<IReturnCode<Interfaces.IDataReader>> RunStoredProcedure(string pDatabaseName, string pStoreProcedureName);
     }
 }
--- Libs/SqlDatabase/SQlite/Wrapper.cs
+++ Libs/SqlDatabase/SQlite/Wrapper.cs
@@ -28,5 +28,60 @@ namespace JB.SqlDatabase.SQlite {
         public Task<IReturnCode<bool>> CreateTable(string pDatabaseName, string pTableName) {
             throw new NotImplementedException();
         }
+
+        public async Task<IReturnCode<Interfaces.IDataReader>> RunQuery(string pDatabaseName, string pQuery) {
+            IReturnCode<Interfaces.IDataReader> rc = new ReturnCode<Interfaces.IDataReader>();
+            Interfaces.IDataReader? dataReader = null;
+
+            try {
+                SqliteConnection connection = new SqliteConnection();
+                await connection.OpenAsync();
+
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = pQuery;
+
+                dataReader = new DataReader(await command.ExecuteReaderAsync());
+            }
+            catch(Exception e) {
+                rc.ErrorCode = 9;
+                ErrorWorker.AddError(rc, rc.ErrorCode, e.Message, e.StackTrace);
+            }
+
+            if (rc.ErrorCode == ErrorCodes.SUCCESS) {
+                rc.Data = dataReader;
+            }
+            
+            return rc;
+        }
+        
+        public async Task<IReturnCode<Interfaces.IDataReader>> RunStoredProcedure(string pDatabaseName, string pStoreProcedureName, IDictionary<string, object> pParameters) {
+            IReturnCode<Interfaces.IDataReader> rc = new ReturnCode<Interfaces.IDataReader>();
+            Interfaces.IDataReader? dataReader = null;
+
+            try {
+                SqliteConnection connection = new SqliteConnection();
+                await connection.OpenAsync();
+
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                foreach(KeyValuePair<string, object> pair in pParameters) {
+                    sqlCommand.Parameters.AddWithValue(pair.Key, pair.Value);
+                }
+
+                dataReader = new DataReader(await command.ExecuteReaderAsync());
+            }
+            catch(Exception e) {
+                rc.ErrorCode = 9;
+                ErrorWorker.AddError(rc, rc.ErrorCode, e.Message, e.StackTrace);
+            }
+
+            if (rc.ErrorCode == ErrorCodes.SUCCESS) {
+                rc.Data = dataReader;
+            }
+            
+            return rc;
+        }
+
     }
 }
