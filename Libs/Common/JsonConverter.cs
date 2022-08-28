c42bab39d702f0fd02f5df57247e719e6fc8808b
JackBattye91 <jack.battye@gmail.com>
2022-08-28 19:13:04 +0100

    Added JsonListConverter class

---
+++ Libs/Common/JsonConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JB.Common {
+    public class JsonListConverter<Tinterface, Tclass> : JsonConverter where Tclass : Tinterface {
+        public override bool CanConvert(Type objectType) {
+            return true;
+        }
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer) {
+            IList<Tclass>? classList = serializer.Deserialize<IList<Tclass>>(reader);
+            IList<Tinterface> interfaceList = new List<Tinterface>();
+
+            if (classList == null) {
+                return null;
+            }
+
+            foreach(Tclass classObj in classList) {
+                interfaceList.Add(classObj);
+            }
+
+            return interfaceList;
+        }
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) {
+            serializer.Serialize(writer, value);
+        }
+    }
+}
