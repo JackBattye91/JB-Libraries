16c82bb1c0a64fe8f884f6dc854ff8a96e551f87
JackBattye91 <jack.battye@gmail.com>
2022-08-28 19:39:04 +0100

    Added MimeType consts

---
+++ Libs/Common/Networking/MimeType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Common.Networking {
+    public sealed class MimeType {
+        public static string TEXT = "text/plain";
+        public static string HTML = "text/html";
+        public static string CSS = "text/css";
+        public static string JAVASCRIPT = "text/javascript";
+
+        public static string JSON = "application/json";
+
+        public static string JPEG = "image/jpeg";
+        public static string PNG = "image/png";
+        public static string GIF = "image/gif";
+    }
+}
