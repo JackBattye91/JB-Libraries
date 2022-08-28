d5bc91b3d71424224a6e4724d26851cb284eaa68
JackBattye91 <jack.battye@gmail.com>
2022-08-28 18:59:00 +0100

    Updated Common ErrorWorker class

--- Libs/Common/Errors/ErrorWorker.cs
+++ Libs/Common/Errors/ErrorWorker.cs
@@ -4,31 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JB.Common.Errors {
+namespace JB.Common {
     public class ErrorWorker {
-        public static void AddError<T>(IReturnCode<T> rc, long pErrorCode) {
-            AddError(rc, pErrorCode, null, null);
+        public static void AddError<T>(ReturnCode<T> rc, long pErrorCode) {
+            AddError(rc, pErrorCode);
         }
-        public static void AddError<T>(IReturnCode<T> rc, long pErrorCode, string? pMessage) {
-            AddError(rc, pErrorCode, pMessage, null);
+        public static void AddError<T>(ReturnCode<T> rc, long pErrorCode, string? pMessage) {
+            AddError(rc, pErrorCode, new Exception(pMessage));
         }
-        public static void AddError<T>(IReturnCode<T> rc, long pErrorCode, string? pMessage, string? pStackTrace) {
-            rc.Errors.Add(new Error() {
-                ErrorCode = pErrorCode,
-                Message = pMessage ?? string.Empty,
-                StackTrace = pStackTrace ?? Environment.StackTrace
-            });
+        public static void AddError<T>(ReturnCode<T> rc, long pErrorCode, Exception ex) {
+            rc.Errors.Add(new Error(pErrorCode, ex));
         }
 
-        public static void CopyErrorCode<T, U>(IReturnCode<T>? pSource, IReturnCode<U>? pDestination) {
+        public static void CopyErrors<T, U>(ReturnCode<T>? pSource, ReturnCode<U>? pDestination) {
             if (pSource != null && pDestination != null) {
-                pDestination.ErrorCode = pSource.ErrorCode;
+                pSource.Success = pDestination.Success;
 
                 foreach (Error srcCodes in pSource.Errors) {
                     pDestination?.Errors.Add(srcCodes);
                 }
             }
-            
         }
     }
 }
