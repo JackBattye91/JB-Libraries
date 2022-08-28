b6d5435b6e872c7f0d6ccf2d9317b62d0fec7225
JackBattye91 <jack.battye@gmail.com>
2022-08-28 19:00:03 +0100

    Updated Cosmos IWrapper

--- Libs/Cosmos/IWrapper.cs
+++ Libs/Cosmos/IWrapper.cs
@@ -6,16 +6,16 @@ using Microsoft.Azure.Cosmos;
 
 namespace JB.NoSqlDatabase {
     public interface IWrapper {
-        public Task<JB.Common.Errors.IReturnCode<bool>> CreateDatabase(string pDatabaseId);
+        public Task<JB.Common.ReturnCode<bool>> CreateDatabase(string pDatabaseId);
        
-        public Task<JB.Common.Errors.IReturnCode<Interfaces.IContainer>> GetContainer(string pDatabaseId, string pContainerName);
-        public Task<JB.Common.Errors.IReturnCode<Interfaces.IContainer>> CreateContainer(string pDatabaseId, string pContainerName, string pPartitionKey);
+        public Task<JB.Common.ReturnCode<Interfaces.IContainer>> GetContainer(string pDatabaseId, string pContainerName);
+        public Task<JB.Common.ReturnCode<Interfaces.IContainer>> CreateContainer(string pDatabaseId, string pContainerName, string pPartitionKey);
         
-        public Task<JB.Common.Errors.IReturnCode<T>> GetItem<T>(string pDatebaseId, string pContainerId, string pItemId);
-        public Task<JB.Common.Errors.IReturnCode<IList<T>>> GetItems<T>(string pDatabaseId, string pContainerId);
-        public Task<JB.Common.Errors.IReturnCode<IList<T>>> GetItems<T>(string pDatabaseId, string pContainerId, string pQuery);
-        public Task<JB.Common.Errors.IReturnCode<T>> AddItem<T>(string pDatabaseId, string pContainerId, T pItem);
-        public Task<JB.Common.Errors.IReturnCode<T>> UpdateItem<T>(string pDatabaseId, string pContainerId, T pItem, string pItemId, string pPartionKeyValue);
-        public Task<JB.Common.Errors.IReturnCode<T>> DeleteItem<T>(string pDatabaseId, string pContainerId, string pItemId, string pPartitionKeyValue);     
+        public Task<JB.Common.ReturnCode<T>> GetItem<T>(string pDatebaseId, string pContainerId, string pItemId);
+        public Task<JB.Common.ReturnCode<IList<T>>> GetItems<T>(string pDatabaseId, string pContainerId);
+        public Task<JB.Common.ReturnCode<IList<T>>> GetItems<T>(string pDatabaseId, string pContainerId, string pQuery);
+        public Task<JB.Common.ReturnCode<T>> AddItem<T>(string pDatabaseId, string pContainerId, T pItem);
+        public Task<JB.Common.ReturnCode<T>> UpdateItem<T>(string pDatabaseId, string pContainerId, T pItem, string pItemId, string pPartionKeyValue);
+        public Task<JB.Common.ReturnCode<T>> DeleteItem<T>(string pDatabaseId, string pContainerId, string pItemId, string pPartitionKeyValue);     
     }
 }
