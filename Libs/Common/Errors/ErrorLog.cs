b8b0cd468fb66025f7a832543d96465e4b260ae0
JackBattye91 <jack.battye@gmail.com>
2022-08-29 21:13:37 +0100

    Added ErrorLog class

---
+++ Libs/Common/Errors/ErrorLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Common.Errors {
+    public sealed class ErrorLog {
+        public static IList<Error> Errors { get; private set; } = new List<Error>();
+        public static void Log(Error error) { Errors.Add(error); }
+
+        public static void Save() {
+            if (Errors.Count != 0) {
+                using (StreamWriter writer = File.CreateText("ErrorLog.txt")) {
+                    foreach (Error error in Errors) {
+                        writer.WriteLine($"{error.TimeStamp} - {error.ErrorCode} - {error.Exception?.Message} - {error.Exception?.StackTrace}");
+                    }
+                }
+            }
+        }
+    }
+}
