739d474581b2f5751812873625523a340b534efc
JackBattye91 <jack.battye@gmail.com>
2022-09-25 22:04:02 +0100

    Updated NetworkingError

--- Libs 6.0/Common/Networking/NetworkingError.cs
+++ Libs 6.0/Common/Networking/NetworkingError.cs
@@ -6,25 +6,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace JB.Common.Networking {
-    internal class NetworkingError : IError {
+    internal class NetworkingError : INetworkError {
         public int Scope { get; } = ErrorCodes.SCOPE;
         public int Code { get; set; }
         public Exception? Exception { get; set; }
-        public DateTime TimeStamp { get; protected set; }
+        public DateTime TimeStamp { get; private set; }
         public HttpStatusCode StatusCode { get; set; }
 
-        public NetworkingError(int code, HttpStatusCode status) {
+        public NetworkingError(int code, HttpStatusCode status, Exception? ex = null) {
             Scope = ErrorCodes.SCOPE;
             Code = code;
             TimeStamp = DateTime.UtcNow;
             StatusCode = status;
-        }
-        public NetworkingError(int code, HttpStatusCode status, Exception ex) {
-            Scope = ErrorCodes.SCOPE;
-            Code = code;
             Exception = ex;
-            TimeStamp = DateTime.UtcNow;
-            StatusCode = status;
         }
     }
 }
