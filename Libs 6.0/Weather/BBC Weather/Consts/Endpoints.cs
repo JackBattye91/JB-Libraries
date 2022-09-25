dbc13aaaf5e8314e46ad780473bde8b246a73324
JackBattye91 <jack.battye@gmail.com>
2022-09-25 22:05:14 +0100

    Added EndPoint consts to BBC Weather

---
+++ Libs 6.0/Weather/BBC Weather/Consts/Endpoints.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Weather.BBC_Weather.Consts {
+    internal class Endpoints {
+        public const string BBC_RSS_TODAY_ENDPOINT = "https://weather-broker-cdn.api.bbci.co.uk/en/observation/rss/";
+        public const string BBC_RSS_3DAY_ENDPOINT = "https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/";
+    }
+}
