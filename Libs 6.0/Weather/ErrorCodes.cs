339e0003f4f05f629bf3315f441f714d76015676
JackBattye91 <jack.battye@gmail.com>
2022-09-25 22:05:58 +0100

    Added Weather ErrorCodes

---
+++ Libs 6.0/Weather/ErrorCodes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Weather
+{
+    internal class ErrorCodes
+    {
+        public const int SCOPE = 3;
+
+        public const int GET_TODAYS_FORCAST_FAILED = 1;
+        public const int GET_3DAY_FORCAST_FAILED = 2;
+        public const int EXTRACT_FORCAST_FAILED = 3;
+    }
+}
