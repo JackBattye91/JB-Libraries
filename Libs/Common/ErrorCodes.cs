12b86035efb3954e5844e7cb96282a9d23d41e34
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-09-25 19:47:31 +0100

    Added ErrorCodes

---
+++ Libs/Common/ErrorCodes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Common {
+    public sealed class ErrorCodes {
+        public const int SCOPE = 0;
+
+        public const int BAD_STATUS_CODE_RETURNED = 1;
+        public const int TOKEN_SIGNATURE_DO_NOT_MATCH = 2;
+        public const int SIGNING_TOKEN_FAILED = 3;
+        public const int VALIDATE_TOKEN_FAILED = 4;
+        public const int CONVERT_TO_BASE_64_FAILED = 5;
+        public const int CONVERT_FROM_BASE_64_FAILED = 6;
+    }
+}
