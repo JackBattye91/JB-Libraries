d8f1befb989cbbbb7d9a227ba331405af49209d8
JackBattye91 <jack.battye@gmail.com>
2022-09-25 22:04:53 +0100

    Added NoSqlDatabaseError

---
+++ Libs 6.0/NoSqlDatabase/NoSqlDatabaseError.cs
@@ -0,0 +1,24 @@
+﻿using JB.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.NoSqlDatabase {
+    internal class NoSqlDatabaseError : JB.Common.INetworkError {
+        public int Scope { get; } = ErrorCodes.SCOPE;
+        public int Code { get; set; }
+        public Exception? Exception { get; set; } 
+        public DateTime TimeStamp { get; protected set; }
+        public HttpStatusCode StatusCode { get; set; }
+
+        public NoSqlDatabaseError(int code, HttpStatusCode statusCode, Exception? ex = null) {
+            Code = code;
+            Exception = ex;
+            StatusCode = statusCode;
+            TimeStamp = DateTime.UtcNow;
+        }
+    }
+}
