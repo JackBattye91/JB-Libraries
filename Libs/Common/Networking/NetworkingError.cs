bfea4bd1fd2793ea28cf222dae96453eca76b2d1
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-09-25 19:47:05 +0100

    Added NetworkingError

---
+++ Libs/Common/Networking/NetworkingError.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Common.Networking {
+    internal class NetworkingError : IError {
+        public int Scope { get; } = ErrorCodes.SCOPE;
+        public int Code { get; set; }
+        public Exception? Exception { get; set; }
+        public DateTime TimeStamp { get; protected set; }
+        public HttpStatusCode StatusCode { get; set; }
+
+        public NetworkingError(int code, HttpStatusCode status) {
+            Scope = ErrorCodes.SCOPE;
+            Code = code;
+            TimeStamp = DateTime.UtcNow;
+            StatusCode = status;
+        }
+        public NetworkingError(int code, HttpStatusCode status, Exception ex) {
+            Scope = ErrorCodes.SCOPE;
+            Code = code;
+            Exception = ex;
+            TimeStamp = DateTime.UtcNow;
+            StatusCode = status;
+        }
+    }
+}
