d119c0a5ca45aeae1dc6618fca56fcdf2dd774ba
JackBattye91 <jack.battye@gmail.com>
2022-09-25 22:06:23 +0100

    Added WeatherError

---
+++ Libs 6.0/Weather/WeatherError.cs
@@ -0,0 +1,24 @@
+﻿using JB.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Weather {
+    internal class WeatherError : JB.Common.INetworkError {
+        public HttpStatusCode StatusCode { get; set; }
+        public int Scope { get; } = ErrorCodes.SCOPE;
+        public int Code { get; set; }
+        public Exception? Exception { get; set; }
+        public DateTime TimeStamp { get; protected set; }
+
+        public WeatherError(int code, HttpStatusCode statusCode, Exception? ex) {
+            Code = code;
+            StatusCode = statusCode;
+            Exception = ex;
+            TimeStamp = DateTime.UtcNow;
+        }
+    }
+}
