dbb2bc9290345e28a6ba23e88347d1e0a1cfa448
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-10-07 17:26:48 +0100

    Initial commit

---
+++ SerialCommunication/COM/Wrapper.cs
@@ -0,0 +1,14 @@
+﻿using SerialCommunication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialCommunication.COM {
+    internal class Wrapper : IWrapper {
+        public IList<IDevice> GetDevices() {
+            throw new NotImplementedException();
+        }
+    }
+}
