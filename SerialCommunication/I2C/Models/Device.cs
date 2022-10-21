d40cc632c04931ce3cd8e43d577ee918d4020ad3
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-10-21 14:06:30 +0100

    Added I2C device model

---
+++ SerialCommunication/I2C/Models/Device.cs
@@ -0,0 +1,24 @@
+﻿using SerialCommunication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Device.I2c;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.SerialCommunication.I2C.Models {
+    internal class Device : IDevice {
+        public string Address { get; set; }
+        public string Name { get; set; }
+        public I2cDevice? I2CDevice { get; set; }
+
+        public Device() {
+            Address = Name = string.Empty;
+        }
+        public Device(string pName, I2cDevice pDevice) {
+            I2CDevice = pDevice;
+            Address = I2CDevice.ConnectionSettings.DeviceAddress.ToString();
+            Name = pName;
+        }
+    }
+}
