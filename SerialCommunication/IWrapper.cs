4e554d9c607e1741350db15777779de9894e4d65
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-10-21 14:07:13 +0100

    Updated IWrapper

--- SerialCommunication/IWrapper.cs
+++ SerialCommunication/IWrapper.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JB.Common;
+using SerialCommunication.Interfaces;
 
 namespace SerialCommunication {
     public interface IWrapper {
-        IList<Interfaces.IDevice> GetDevices();
+        IReturnCode<IList<Interfaces.IDevice>> GetDevices();
+        IReturnCode<Interfaces.IDevice> AddDevice(string pAddress);
+        IReturnCode<bool> WriteData(byte[] pData, IDevice pDevice);
+        IReturnCode<byte[]> ReadData(IDevice pDevice);
     }
 }
