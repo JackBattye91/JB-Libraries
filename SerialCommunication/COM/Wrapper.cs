2b60078124973a3cb5bc038dea09bb621cdc108f
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-10-21 14:06:22 +0100

    Updated COM wrapper

--- SerialCommunication/COM/Wrapper.cs
+++ SerialCommunication/COM/Wrapper.cs
@@ -1,4 +1,5 @@
-﻿using SerialCommunication.Interfaces;
+﻿using JB.Common;
+using SerialCommunication.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace SerialCommunication.COM {
     internal class Wrapper : IWrapper {
-        public IList<IDevice> GetDevices() {
+        public IReturnCode<IDevice> AddDevice(string pAddress) {
+            throw new NotImplementedException();
+        }
+
+        public IReturnCode<byte[]> ReadData(IDevice pDevice) {
+            throw new NotImplementedException();
+        }
+
+        public IReturnCode<bool> WriteData(byte[] pData, IDevice pDevice) {
+            throw new NotImplementedException();
+        }
+
+        IReturnCode<IList<IDevice>> IWrapper.GetDevices() {
             throw new NotImplementedException();
         }
     }
