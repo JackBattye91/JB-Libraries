adc8ca602366fa2d0d98e0a88a4a4bd72a1af989
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-10-22 09:56:55 +0100

    Added COM ErrorCodes

---
+++ SerialCommunication/COM/ErrorCodes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.SerialCommunication.COM {
+    internal class ErrorCodes {
+        public const int SCOPE = 5;
+
+        public const int ADD_DEVICE_FAILED = 1;
+        public const int READ_DATA_FAILED = 2;
+        public const int WRTIE_DATA_FAILED = 3;
+        public const int GET_DEVICES_FAILED = 4;
+        public const int UNABLE_TO_OPEN_PORT = 5;
+    }
+}
