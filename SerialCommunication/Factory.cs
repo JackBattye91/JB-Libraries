222a9d81c4f41962cf2bba2bd167ac02d8b4f2fb
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-10-22 10:47:42 +0100

    Updated Factory

--- SerialCommunication/Factory.cs
+++ SerialCommunication/Factory.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SerialCommunication {
+namespace JB.SerialCommunication {
     public sealed class Factory {
-        public static IWrapper CreateSerialCommunicationInstance() {
+        public static IWrapper CreateDefaultSerialCommunicationInstance() {
+            return new COM.Wrapper();
+        }
+        public static IWrapper CreateI2CSerialCommunicationInstance() {
+            return new I2C.Wrapper();
+        }
+        public static IWrapper CreateCOMSerialCommunicationInstance() {
             return new COM.Wrapper();
         }
     }
