825c91b82199919b83ea3624da8fcd095483713e
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-02-07 17:56:19 +0000

    Added Factory

---
+++ Libs/SqlDatabase/Factory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.SqlDatabase {
+    public sealed class Factory {
+        public static IWrapper CreateSqlWrapperInstance() {
+            return new SQlite.Wrapper();
+        }
+    }
+}
