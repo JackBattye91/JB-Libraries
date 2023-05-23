ec725daa6f80e371c3d202fc28e6e1c4ebb37ad8
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-05-23 10:05:07 +0100

    Updated CalendarError

--- Libs/Calendar/CalendarError.cs
+++ Libs/Calendar/CalendarError.cs
@@ -9,14 +9,14 @@ using System.Threading.Tasks;
 namespace JB.Calendar {
     internal class CalendarError : JB.Common.IError {
         public int Scope { get; } = ErrorCodes.SCOPE;
-        public int Code { get; set; }
+        public int ErrorCode { get; set; }
         public Exception? Exception { get; set; }
         public DateTime TimeStamp { get; protected set; }
         public HttpStatusCode StatusCode { get; set; }
 
-        public CalendarError(int code, Exception? exception = null) {
-            Code = code;
-            Exception = exception;
+        public CalendarError(int pErrorCode, Exception? pException = null) {
+            ErrorCode = pErrorCode;
+            Exception = pException;
             TimeStamp = DateTime.UtcNow;
             StatusCode = HttpStatusCode.OK;
         }
