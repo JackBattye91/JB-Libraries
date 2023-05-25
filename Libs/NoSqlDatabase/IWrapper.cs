c9ceca39253cdbb0ab057a09f88c91df04a6be3c
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-05-25 19:19:33 +0100

    Update IWrapper.cs

--- Libs/NoSqlDatabase/IWrapper.cs
+++ Libs/NoSqlDatabase/IWrapper.cs
@@ -11,12 +11,20 @@ namespace JB.NoSqlDatabase {
        
         public Task<IReturnCode<Interfaces.IContainer>> GetContainer(string pDatabaseId, string pContainerName);
         public Task<IReturnCode<Interfaces.IContainer>> CreateContainer(string pDatabaseId, string pContainerName, string pPartitionKey);
+
+        public Task<IReturnCode<Tinterface>> GetItem<Tinterface>(string pDatebaseId, string pContainerId, string pItemId);
+        public Task<IReturnCode<Tinterface>> GetItem<Tinterface, Tmodel>(string pDatebaseId, string pContainerId, string pItemId) where Tmodel : class, Tinterface;
+
+        public Task<IReturnCode<IList<Tinterface>>> GetItems<Tinterface>(string pDatabaseId, string pContainerId, string pQuery);
+        public Task<IReturnCode<IList<Tinterface>>> GetItems<Tinterface, Tmodel>(string pDatabaseId, string pContainerId, string pQuery) where Tmodel : Tinterface;
+
+        public Task<IReturnCode<IList<Tinterface>>> GetItems<Tinterface>(string pDatabaseId, string pContainerId);
+        public Task<IReturnCode<IList<Tinterface>>> GetItems<Tinterface, Tmodel>(string pDatabaseId, string pContainerId) where Tmodel : Tinterface;
         
-        public Task<IReturnCode<T>> GetItem<T>(string pDatebaseId, string pContainerId, string pItemId);
-        public Task<IReturnCode<IList<T>>> GetItems<T>(string pDatabaseId, string pContainerId);
-        public Task<IReturnCode<IList<T>>> GetItems<T>(string pDatabaseId, string pContainerId, string pQuery);
         public Task<IReturnCode<T>> AddItem<T>(string pDatabaseId, string pContainerId, T pItem);
+        public Task<IReturnCode<Tinterface>> AddItem<Tinterface, Tmodel>(string pDatabaseId, string pContainerId, Tinterface pItem) where Tmodel : class, Tinterface;
         public Task<IReturnCode<T>> UpdateItem<T>(string pDatabaseId, string pContainerId, T pItem, string pItemId, string pPartionKeyValue);
+        public Task<IReturnCode<Tinterface>> UpdateItem<Tinterface, Tmodel>(string pDatabaseId, string pContainerId, Tinterface pItem, string pItemId, string pPartionKeyValue) where Tmodel : class, Tinterface;
         public Task<IReturnCode<bool>> DeleteItem<T>(string pDatabaseId, string pContainerId, string pItemId, string pPartitionKeyValue);     
     }
 }
