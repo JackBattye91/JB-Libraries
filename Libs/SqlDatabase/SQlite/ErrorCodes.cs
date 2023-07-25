8297c7cbe7c6b06b267b18e931420f8251964913
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-07-25 19:03:28 +0100

    Update ErrorCodes.cs

--- Libs/SqlDatabase/SQlite/ErrorCodes.cs
+++ Libs/SqlDatabase/SQlite/ErrorCodes.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace JB.SqlDatabase.SQlite {
     internal class ErrorCodes {
-        internal const int SCOPE = 5;
+        private const int SCOPE = 5;
 
-        public const int PARSE_DATA_FAILED = 1;
-        public const int CREATE_TABLE_FAILED = 2;
-        public const int RUN_QUERY_FAILED = 3;
-        public const int RUN_STORE_PROCEDURE_FAILED = 4;
-        public const int GET_DATA_FAILED = 5;
-        public const int UNABLE_TO_OPEN_DATA_BASE = 6;
+        public const int PARSE_DATA_FAILED = (SCOPE * JB.Common.ErrorCodes.SCOPE_OFFSET) + 1;
+        public const int CREATE_TABLE_FAILED = (SCOPE * JB.Common.ErrorCodes.SCOPE_OFFSET) + 2;
+        public const int RUN_QUERY_FAILED = (SCOPE * JB.Common.ErrorCodes.SCOPE_OFFSET) + 3;
+        public const int RUN_STORE_PROCEDURE_FAILED = (SCOPE * JB.Common.ErrorCodes.SCOPE_OFFSET) + 4;
+        public const int GET_DATA_FAILED = (SCOPE * JB.Common.ErrorCodes.SCOPE_OFFSET) + 5;
+        public const int UNABLE_TO_OPEN_DATA_BASE = (SCOPE * JB.Common.ErrorCodes.SCOPE_OFFSET) + 6;
     }
 }
