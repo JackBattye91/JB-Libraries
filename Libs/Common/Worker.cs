979f82dd35d801b6061774de7cc016e4bbe09d6b
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-07-31 12:11:43 +0100

    Added Worker

---
+++ Libs/Common/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Common {
+    public sealed class Worker {
+        public static IList<Tinterface> ConvertToInterfaceList<Tinterface, Tobject>(IList<Tobject> pObjectList) where Tobject : Tinterface {
+            IList<Tinterface> interfaceList = new List<Tinterface>();
+
+            foreach(Tobject obj in pObjectList) {
+                interfaceList.Add(obj);
+            }
+
+            return interfaceList;
+        }
+    }
+}
