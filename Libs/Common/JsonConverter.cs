debcdc859b502a11877ab04fb541aeedcb1b2880
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-08-08 15:32:02 +0100

    Update JsonConverters with Enum converters

--- Libs/Common/JsonConverter.cs
+++ Libs/Common/JsonConverter.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace JB.Common {
     public class JsonListConverter<Tinterface, Tclass> : JsonConverter where Tclass : Tinterface {
@@ -28,6 +23,44 @@ namespace JB.Common {
 
         public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) {
             serializer.Serialize(writer, value);
+        }
+    }
+
+    public class EnumIntConverter<T> : JsonConverter {
+        public override bool CanConvert(Type objectType) {
+            return objectType.IsEnum;
+        }
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer) {
+            int value = serializer.Deserialize<int>(reader);
+            T? obj = (T?)typeof(T).GetEnumValues().GetValue(value);
+            return obj;
+        }
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) {
+            serializer.Serialize(writer, (int?)value);
+        }
+    }
+    public class EnumNameConverter<T> : JsonConverter<T> {
+        public override T? ReadJson(JsonReader reader, Type objectType, T? existingValue, bool hasExistingValue, JsonSerializer serializer) {
+            string? value = serializer.Deserialize<string>(reader);
+            string[] enumNamesList = objectType.GetEnumNames();
+            T[] enumValuesList = (T[])objectType.GetEnumValues();
+
+            for(int n = 0; n < enumNamesList.Length; n++) {
+                if (enumNamesList[n] == value) {
+                    return enumValuesList[n];
+                }
+            }
+
+            return default;
+        }
+
+        public override void WriteJson(JsonWriter writer, T? value, JsonSerializer serializer) {
+            if (value is T) {
+                string? name = typeof(T).GetEnumName(value);
+                serializer.Serialize(writer, name);
+            }
         }
     }
 }
