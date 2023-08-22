00c61f3afda8145d3856d6c3237ed5978bab49ec
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-08-22 17:27:42 +0100

    Updated Worker with PopulateObjects and GetObjectValues

--- Libs/SqlDatabase/SQlite/Worker.cs
+++ Libs/SqlDatabase/SQlite/Worker.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using JB.Common;
+using JB.SqlDatabase.Interfaces;
 
 namespace JB.SqlDatabase.SQlite {
     internal class Worker {
@@ -36,8 +38,70 @@ namespace JB.SqlDatabase.SQlite {
             if (type == typeof(byte)) {
                 return "SMALLINT";
             }
+            else if (type == typeof(string)) {
+                return "TEXT";
+            }
 
             return string.Empty;
+        }
+
+
+        public static IReturnCode<IList<T?>> PopulateObjects<T>(IDataReader pDataReader) {
+            IReturnCode<IList<T?>> rc = new ReturnCode<IList<T?>>();
+            IList<T?> objList = new List<T?>();
+
+            try {
+                var properties = typeof(T).GetProperties();
+                
+                while(pDataReader.NextRow()) {
+                    T? obj = default;
+                    obj = (T?)Activator.CreateInstance(typeof(T));
+
+                    foreach (var prop in properties) {
+                        if (pDataReader.HasValue(prop.Name)) {
+                            object value = pDataReader.Get(prop.Name);
+                            prop.SetValue(obj, value);
+                        }
+                    }
+
+                    objList.Add(obj);
+                }
+            }
+            catch(Exception ex) {
+                rc.ErrorCode = ErrorCodes.POPULATE_OBJECT_FAILED;
+                rc.Errors.Add(new Error(rc.ErrorCode, ex));
+            }
+
+            if (rc.Success) {
+                rc.Data = objList;
+            }
+
+            return rc;
+        }
+
+        public static IReturnCode<IDictionary<string, object?>> GetObjectValues<T>(T pObj) {
+            IReturnCode<IDictionary<string, object?>> rc = new ReturnCode<IDictionary<string, object?>>();
+            IDictionary<string, object?> valuesMap = new Dictionary<string, object?>();
+
+            try {
+                if (rc.Success) {
+                    PropertyInfo[] properties = typeof(T).GetProperties();
+
+                    foreach (var prop in properties) {
+                        valuesMap.Add(prop.Name, prop.GetValue(pObj));
+                    }
+                }
+            }
+            catch (Exception ex) {
+                rc.ErrorCode = ErrorCodes.GET_OBJECT_VALUES_FAILED;
+                rc.Errors.Add(new Error(rc.ErrorCode, ex));
+            }
+
+            if (rc.Success) {
+                rc.Data = valuesMap;
+            }
+
+            return rc;
         }
     }
 }
