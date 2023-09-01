c723cb30d3e8fbe1bf1abfb84165802ae745c930
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-09-01 10:12:35 +0100

    Added TableAttribute

---
+++ Libs/SqlDatabase/Attributes/TableAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.SqlDatabase.Attributes {
+    [AttributeUsage(AttributeTargets.Property)]
+    public class TableAttribute : Attribute {
+        public string TableName { get; set; }
+        public string ColumnName { get; set; }
+
+        public TableAttribute(string pTableName, string pColumnName) {
+            TableName = pTableName;
+            ColumnName = pColumnName;
+        }
+    }
+}
