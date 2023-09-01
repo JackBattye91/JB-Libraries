42005eea2fd6ae50fb7efffb997a894105dee978
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-09-01 10:13:04 +0100

    Updated SQLite Worker with alternate PopulateObjects

--- Libs/SqlDatabase/SQlite/Worker.cs
+++ Libs/SqlDatabase/SQlite/Worker.cs
@@ -34,40 +34,86 @@ namespace JB.SqlDatabase.SQlite {
             return rc;
         }
 
-        public static string ConvertToDatabaseType(Type type) {
-            if (type == typeof(byte)) {
-                return "SMALLINT";
-            }
-            else if (type == typeof(string)) {
+        public static string? ConvertToDatabaseType(Type type) {
+            if (type == typeof(string)) {
                 return "TEXT";
             }
+            else if (type == typeof(int) || type == typeof(long) || type == typeof(byte) || type == typeof(bool) || type.IsEnum) {
+                return "INTEGER";
+            }
+            else if (type == typeof(float) || type == typeof(double)) {
+                return "REAL";
+            }
 
-            return string.Empty;
+            return null;
         }
-
 
         public static IReturnCode<IList<T?>> PopulateObjects<T>(IDataReader pDataReader) {
             IReturnCode<IList<T?>> rc = new ReturnCode<IList<T?>>();
             IList<T?> objList = new List<T?>();
 
             try {
-                var properties = typeof(T).GetProperties();
-                
-                while(pDataReader.NextRow()) {
-                    T? obj = default;
-                    obj = (T?)Activator.CreateInstance(typeof(T));
+                if (rc.Success) {
+                    IReturnCode<IList<object?>> popObject = PopulateObjects(pDataReader, typeof(T));
+
+                    if (popObject.Success) {
+                        foreach(var obj in popObject.Data!) {
+                            if (obj is T) {
+                                objList.Add((T?)obj);
+                            }
+                        }
+                    }
+                    if (popObject.Failed) {
+                        ErrorWorker.CopyErrors(popObject, rc);
+                    }
+                }
+            }
+            catch(Exception ex) {
+                rc.ErrorCode = ErrorCodes.POPULATE_OBJECT_FAILED;
+                rc.Errors.Add(new Error(rc.ErrorCode, ex));
+            }
+
+            if (rc.Success) {
+                rc.Data = objList;
+            }
+
+            return rc;
+        }
+        public static IReturnCode<IList<object?>> PopulateObjects(IDataReader pDataReader, Type pObjectType) {
+            IReturnCode<IList<object?>> rc = new ReturnCode<IList<object?>>();
+            IList<object?> objList = new List<object?>();
+
+            try {
+                var properties = pObjectType.GetProperties();
+
+                while (pDataReader.NextRow()) {
+                    object? obj = default;
+                    obj = Activator.CreateInstance(pObjectType);
 
                     foreach (var prop in properties) {
                         if (pDataReader.HasValue(prop.Name)) {
                             object value = pDataReader.Get(prop.Name);
-                            prop.SetValue(obj, value);
+
+                            if (prop.PropertyType == typeof(int)) {
+                                prop.SetValue(obj, Convert.ToInt32(value));
+                            }
+                            else if (prop.PropertyType.IsEnum) {
+                                int intValue = Convert.ToInt32(value);
+                                prop.SetValue(obj, prop.PropertyType.GetEnumValues().GetValue(intValue));
+                            }
+                            else if (prop.PropertyType == typeof(string)) {
+                                prop.SetValue(obj, (string)value);
+                            }
+                            else if (prop.PropertyType == typeof(bool)) {
+                                prop.SetValue(obj, (long)value != 0);
+                            }
                         }
                     }
 
                     objList.Add(obj);
                 }
             }
-            catch(Exception ex) {
+            catch (Exception ex) {
                 rc.ErrorCode = ErrorCodes.POPULATE_OBJECT_FAILED;
                 rc.Errors.Add(new Error(rc.ErrorCode, ex));
             }
