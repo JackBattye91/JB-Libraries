771ba8a43f209f148371ba672878299615592164
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-09-14 16:03:07 +0100

    Added ItemTag consts to BBC Weather

---
+++ Libs/Weather/BBC Weather/Consts/ItemTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Weather.BBC_Weather.Consts {
+    internal class ItemTag {
+        public const string Title = "title";
+        public const string Link = "link";
+        public const string Description = "description";
+        public const string PubDate = "pubDate";
+        public const string Date = "dc:date";
+        public const string GeoPos = "georss:point";
+    }
+}
