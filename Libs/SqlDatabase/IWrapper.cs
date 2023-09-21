2571fb319286126021870efa752541700bdee530
JackBattye91 <jack.battye@gmail.com>
2023-09-21 15:31:42 +0100

    Update IWrapper.cs

--- Libs/SqlDatabase/IWrapper.cs
+++ Libs/SqlDatabase/IWrapper.cs
@@ -12,7 +12,7 @@ namespace JB.SqlDatabase {
         Task<IReturnCode<bool>> DeleteTable(string pDatabaseName, string pTableName);
 
         Task<IReturnCode<Interfaces.IDataReader>> RunQuery(string pDatabaseName, string pQuery);
-        Task<IReturnCode<Interfaces.IDataReader>> RunStoredProcedure(string pDatabaseName, string pStoreProcedureName, IDictionary<string, object> pParameters);
+        Task<IReturnCode<Interfaces.IDataReader>> RunStoredProcedure(string pDatabaseName, string pStoreProcedureName, IDictionary<string, object?> pParameters);
         
         Task<IReturnCode<IList<T>>> Get<T>(string pDatabaseName, string pTableName, string? pQueryParameters = null);
         Task<IReturnCode<T>> Insert<T>(string pDatabaseName, string pTableName, T pItem);
