fc42e451869382ef775d3807ce1c4fce196416be
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-09-28 12:26:28 +0100

    Added DataReader model to MsSql

---
+++ Libs/SqlDatabase/MsSql/Models/DataReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace JB.SqlDatabase.MsSql.Models {
+    internal class DataReader : SqlDatabase.Interfaces.IDataReader {
+        SqlDataReader SqlDataReader;
+
+        public DataReader(SqlDataReader pSqlDataReader) {
+            SqlDataReader = pSqlDataReader;
+        }
+
+        public object Get(string pName) {
+            int ordinal = SqlDataReader.GetOrdinal(pName);
+            return SqlDataReader.GetValue(ordinal);
+        }
+
+        public object Get(int pOrdinal) {
+            return SqlDataReader.GetValue(pOrdinal);
+        }
+
+        public int GetOrdinal(string pName) {
+            return SqlDataReader.GetOrdinal(pName);
+        }
+
+        public bool HasRows() {
+            return SqlDataReader.HasRows;
+        }
+
+        public bool HasValue(string pName) {
+            return Get(pName) != null;
+        }
+
+        public bool NextRow() {
+            return SqlDataReader.NextResult();
+        }
+    }
+}
