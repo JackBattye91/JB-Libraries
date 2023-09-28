cd9e9aab18ae4807e3dc1c9134f90ebd098dc189
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-09-28 12:26:15 +0100

    Added EnviromentVariables consts to MsSql

---
+++ Libs/SqlDatabase/MsSql/Consts/EnvironmentVariables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.SqlDatabase.MsSql.Consts {
+    public sealed class EnvironmentVariables {
+        public const string ConnectionString = "sql-connection-string";
+    }
+}
