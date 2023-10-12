738b995330db7e6172658f32cab9428ebb0b74cd
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-10-12 10:06:02 +0100

    Added IConverter to Sql

---
+++ Libs/SqlDatabase/Converters/IConverter.cs
@@ -0,0 +1,14 @@
+﻿using JB.SqlDatabase.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.SqlDatabase.Converters {
+    public interface IConverter {
+        bool CanConvert(Type pType);
+        object? Read(IDataReader pReader);
+        void Write(IDataWriter pWriter, object? pObject);
+    }
+}
