c75e90702380298ae25e8ee6140ace44e3cc2a53
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-01-09 13:56:07 +0000

    Updated BBC Weather Worker with StringExtensions

--- Libs/Weather/BBC Weather/Worker.cs
+++ Libs/Weather/BBC Weather/Worker.cs
@@ -1,13 +1,8 @@
 ﻿using JB.Common;
 using JB.Weather.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Xml;
+using JB.Common.Utilities;
 
 namespace JB.Weather.BBC_Weather {
     internal class Worker {
