4fe90fa28ec172325046342ed5c1a2b205b170d6
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-01-25 09:39:01 +0000

    Updated IWrapper to allow entry IReturnCode

--- Libs/NoSqlDatabase/IWrapper.cs
+++ Libs/NoSqlDatabase/IWrapper.cs
@@ -2,12 +2,11 @@
 using System.Text;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.Azure.Cosmos;
 using JB.Common;
 
 namespace JB.NoSqlDatabase {
     public interface IWrapper {
-        public Task<IReturnCode<bool>> CreateDatabase(string pDatabaseId);
+        public Task<IReturnCode> CreateDatabase(string pDatabaseId);
        
         public Task<IReturnCode<Interfaces.IContainer>> GetContainer(string pDatabaseId, string pContainerName);
         public Task<IReturnCode<Interfaces.IContainer>> CreateContainer(string pDatabaseId, string pContainerName, string pPartitionKey);
@@ -25,6 +24,6 @@ namespace JB.NoSqlDatabase {
         public Task<IReturnCode<Tinterface>> AddItem<Tinterface, Tmodel>(string pDatabaseId, string pContainerId, Tinterface pItem) where Tmodel : class, Tinterface;
         public Task<IReturnCode<Tmodel>> UpdateItem<Tmodel>(string pDatabaseId, string pContainerId, Tmodel pItem, string pItemId, string pPartionKeyValue);
         public Task<IReturnCode<Tinterface>> UpdateItem<Tinterface, Tmodel>(string pDatabaseId, string pContainerId, Tinterface pItem, string pItemId, string pPartionKeyValue) where Tmodel : class, Tinterface;
-        public Task<IReturnCode<bool>> DeleteItem<Tmodel>(string pDatabaseId, string pContainerId, string pItemId, string pPartitionKeyValue);
+        public Task<IReturnCode> DeleteItem<Tmodel>(string pDatabaseId, string pContainerId, string pItemId, string pPartitionKeyValue);
     }
 }
