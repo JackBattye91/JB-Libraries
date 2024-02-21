ade976efc930bb651623b502c3a92b04b1710929
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-02-21 08:17:26 +0000

    Workflow test

--- Libs/NoSqlDatabase/Factory.cs
+++ Libs/NoSqlDatabase/Factory.cs
@@ -1,4 +1,9 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace JB.NoSqlDatabase {
     public class Factory {
         public static IWrapper CreateNoSqlDatabaseWrapper(string? pConnectionString = null) {
