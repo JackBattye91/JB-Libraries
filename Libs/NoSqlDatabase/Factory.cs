3f1165deb4af17f16c8dc365f6648ce190bca1bf
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-02-21 08:08:50 +0000

    Updated NoSql Factory

--- Libs/NoSqlDatabase/Factory.cs
+++ Libs/NoSqlDatabase/Factory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JB.NoSqlDatabase {
+﻿namespace JB.NoSqlDatabase {
     public class Factory {
         public static IWrapper CreateNoSqlDatabaseWrapper(string? pConnectionString = null) {
             return new Cosmos.Wrapper(pConnectionString);
