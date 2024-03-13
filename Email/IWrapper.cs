a6aa08dbcd64f8556187e1de2494f4ad186e7f7d
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-03-13 11:03:09 +0000

    Initial commit

---
+++ Email/IWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Email.Interfaces;
+using JB.Common;
+
+namespace JB.Email
+{
+    public interface IWrapper
+    {
+        Task<IReturnCode> Send(IEmailHeader pHeader, IEmailBody pBody);
+    }
+}
