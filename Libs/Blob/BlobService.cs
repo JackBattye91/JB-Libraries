689236e96f510a6fafdba06c2062a6b3dc2c0ff7
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-07-18 14:40:36 +0100

    Updated BlobService to remove unused classes

--- Libs/Blob/BlobService.cs
+++ Libs/Blob/BlobService.cs
@@ -9,38 +9,11 @@ using System.Threading.Tasks;
 
 namespace JB.Blob
 {
-    public interface IBlobService
-    {
-        Task<IReturnCode<string>> UploadBlobAsync(byte[] pData, string pContainerName, string pFileName);
-        Task<IReturnCode<string>> UploadBlobAsync(Stream pStream, string pContainerName, string pFileName);
-        Task<IReturnCode<Stream>> GetBlobAsStreamAsync(string pContainerName, string pFileName);
-        Task<IReturnCode<byte[]>> GetBlobAsArrayAsync(string pContainerName, string pFileName);
-    }
-
-    public class BlobService : IBlobService
-    {
-        private readonly IWrapper _wrapper;
-        public BlobService(string? pConnectString)
-        {
-            _wrapper = Factory.CreateBlobWrapper(pConnectString);
-        }
-
-        public async Task<IReturnCode<string>> UploadBlobAsync(byte[] pData, string pContainerName, string pFileName) => 
-            await _wrapper.UploadBlobAsync(pData, pContainerName, pFileName);
-        public async Task<IReturnCode<string>> UploadBlobAsync(Stream pStream, string pContainerName, string pFileName) =>
-            await _wrapper.UploadBlobAsync(pStream, pContainerName, pFileName);
-        public async Task<IReturnCode<Stream>> GetBlobAsStreamAsync(string pContainerName, string pFileName) =>
-            await _wrapper.GetBlobAsStreamAsync(pContainerName, pFileName);
-        public async Task<IReturnCode<byte[]>> GetBlobAsArrayAsync(string pContainerName, string pFileName) =>
-            await _wrapper.GetBlobAsArrayAsync(pContainerName, pFileName);
-    }
-
     public static class BlobServiceExtension
     {
         public static IServiceCollection AddBlobService(this IServiceCollection services, string? connectionString = null)
         {
-            services.AddTransient<IBlobService, BlobService>((inter) => { return new BlobService(connectionString); });
-            return services;
+            return services.AddScoped<IWrapper>((provider) => { return Factory.CreateBlobWrapper(connectionString); });
         }
     }
 }
