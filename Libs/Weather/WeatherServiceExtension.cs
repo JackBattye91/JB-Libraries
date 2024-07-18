55960a7ad24024987a96bdbad5ed014db660f813
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-07-18 14:50:16 +0100

    Added WeatherServiceExtension

---
+++ Libs/Weather/WeatherServiceExtension.cs
@@ -0,0 +1,19 @@
+﻿using JB.Common;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JB.Weather
+{
+    public static class WeatherServiceExtension
+    {
+        public static IServiceCollection AddWeatherService(this IServiceCollection services)
+        {
+            return services.AddScoped<IWrapper>((provider) => { return Factory.CreateWeatherWrapper(); });
+        }
+    }
+}
