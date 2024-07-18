c2fbb94f2f2baa675353e3edae7dfe5ccc7dc15b
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-07-18 14:26:00 +0100

    Fixed issues in SQlite wrapper

--- Libs/SqlDatabase/SQlite/Wrapper.cs
+++ Libs/SqlDatabase/SQlite/Wrapper.cs
@@ -355,7 +355,6 @@ namespace JB.SqlDatabase.SQlite {
             return rc;
         }
 
-
         protected static SqliteConnection CreateConnection(string pDatabaseName, string pVersion = "3") {
             if (!File.Exists(pDatabaseName)) {
                 throw new Exception("Database file does not exist");
@@ -718,117 +717,6 @@ namespace JB.SqlDatabase.SQlite {
 
             if (rc.Success) {
                 rc.Data = objList;
-            }
-
-            return rc;
-        }
-        public async Task<IReturnCode<T>> Insert<T>(string pDatabaseName, string pTableName, T pObject) {
-            IReturnCode<T> rc = new ReturnCode<T>();
-            Interfaces.IDataReader? dataReader = null;
-            T? item = default;
-            IDictionary<string, object> properties = new Dictionary<string, object>();
-
-            try {
-                if (rc.Success) {
-
-                }
-
-                if (rc.Success) {
-                    IReturnCode<Interfaces.IDataReader> dataReaderRc = await RunQuery(pDatabaseName, $"INSERT INTO {pTableName}() VALUES ");
-
-                    if (dataReaderRc.Success) {
-                        dataReader = dataReaderRc.Data;
-
-                        while (dataReader?.NextRow() ?? false) {
-                            IReturnCode<T> parseDataRc = Worker.ParseData<T>(dataReader);
-
-                            if (parseDataRc.Success) {
-                                item = parseDataRc.Data;
-                            }
-                            if (parseDataRc.Failed) {
-                                ErrorWorker.CopyErrors(parseDataRc, rc);
-                            }
-                        }
-                    }
-                    else {
-                        ErrorWorker.CopyErrors(dataReaderRc, rc);
-                    }
-                }
-
-                if (rc.Success) {
-                    PropertyInfo[] propInfo = typeof(T).GetProperties();
-                    foreach (PropertyInfo prop in propInfo) {
-                        prop.SetValue(item, 0);
-                    }
-                }
-            }
-            catch (Exception ex) {
-                rc.Errors.Add(new SqlDatabaseError(ErrorCodes.INSERT_DATA_FAILED, ex));
-            }
-
-            return rc;
-        }
-        public async Task<IReturnCode<bool>> Delete<T>(string pDatabaseName, string pTableName, string pId) {
-            IReturnCode<bool> rc = new ReturnCode<bool>();
-
-            try {
-                if (rc.Success) {
-                    IReturnCode<Interfaces.IDataReader> dataReaderRc = await RunQuery(pDatabaseName, $"DELETE FROM {pTableName} WHERE id = '{pId}'");
-
-                    if (dataReaderRc.Failed) {
-                        ErrorWorker.CopyErrors(dataReaderRc, rc);
-                    }
-                }
-            }
-            catch (Exception ex) {
-                rc.Errors.Add(new SqlDatabaseError(ErrorCodes.DELETE_DATA_FAILED, ex));
-            }
-
-            return rc;
-        }
-        public async Task<IReturnCode<T>> Update<T>(string pDatabaseName, string pTableName, T pObject) {
-            IReturnCode<T> rc = new ReturnCode<T>();
-            Interfaces.IDataReader? dataReader = null;
-            T? item = default;
-            StringBuilder valuesBuilder = new StringBuilder();
-
-            try {
-
-                if (rc.Success) {
-
-                }
-
-                if (rc.Success) {
-                    IReturnCode<Interfaces.IDataReader> dataReaderRc = await RunQuery(pDatabaseName, $"UPDATE {pTableName} SET {valuesBuilder} WHERE id = '{pObject}'");
-
-                    if (dataReaderRc.Success) {
-                        dataReader = dataReaderRc.Data;
-
-                        while (dataReader?.NextRow() ?? false) {
-                            IReturnCode<T> parseDataRc = Worker.ParseData<T>(dataReader);
-
-                            if (parseDataRc.Success) {
-                                item = parseDataRc.Data;
-                            }
-                            if (parseDataRc.Failed) {
-                                ErrorWorker.CopyErrors(parseDataRc, rc);
-                            }
-                        }
-                    }
-                    else {
-                        ErrorWorker.CopyErrors(dataReaderRc, rc);
-                    }
-                }
-
-                if (rc.Success) {
-                    PropertyInfo[] propInfo = typeof(T).GetProperties();
-                    foreach (PropertyInfo prop in propInfo) {
-                        prop.SetValue(item, 0);
-                    }
-                }
-            }
-            catch (Exception ex) {
-                rc.Errors.Add(new SqlDatabaseError(ErrorCodes.UPDATE_DATA_FAILED, ex));
             }
 
             return rc;
